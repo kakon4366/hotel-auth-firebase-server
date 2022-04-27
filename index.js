1d3d765fcfad8c7460a22b35ad039d33624c9253
kakon4366 <kakonbarman4366@gmail.com>
2022-04-27 22:01:13 +0600

    services post and get success

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
 const express = require("express");
-const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lry7n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+async function run() {
+	try {
+		await client.connect();
+		const serviceCollection = client
+			.db("hotelFirebase")
+			.collection("services");
+
+		//post method (add servicess)
+		app.post("/service", async (req, res) => {
+			const service = req.body;
+			const result = await serviceCollection.insertOne(service);
+			res.send(`${result} Successful add services`);
+		});
+
+		//get method (get service)
+		app.get("/service", async (req, res) => {
+			const query = {};
+			const cursor = serviceCollection.find(query);
+			const result = await cursor.toArray();
+			res.send(result);
+		});
+	} finally {
+		// await client.close();
+	}
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
 	res.send("Hotel Auth Firebase Servier is Runngi");
