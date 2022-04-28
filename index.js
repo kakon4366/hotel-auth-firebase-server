eeeb49c07e91db41f262ebe67920edba807755cf
kakon4366 <kakonbarman4366@gmail.com>
2022-04-28 13:38:49 +0600

    jwt install and access token genarete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+var jwt = require("jsonwebtoken");
 require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
@@ -24,6 +25,17 @@ async function run() {
 
 		const orderCollection = client.db("hotelFirebase").collection("orders");
 
+		//require('crypto').randomBytes(64).toString('hex')
+		//AUTH
+		app.post("/login", async (req, res) => {
+			const user = req.body;
+			const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
+				expiresIn: "1d",
+			});
+			res.send({ accessToken });
+		});
+
+		//SERVICE
 		//post method (add servicess)
 		app.post("/service", async (req, res) => {
 			const service = req.body;
